081b97d4d90e03151ca926111eaef6843765768e
Benno van den Berg <bennovandenberg@gmail.com>
2023-05-10 16:02:31 +0200

    wip

--- user_service/src/commands/start.rs
+++ user_service/src/commands/start.rs
@@ -1,9 +1,17 @@
 use crate::handlers;
 use anyhow::{Context, Result};
+use axum::body::HttpBody;
+use axum::response::Response;
 use axum::routing::{get, post};
 use axum::Router;
 use clap::Parser;
+use http::{Request, StatusCode};
+use std::error::Error;
+use std::future::Future;
 use std::net::SocketAddr;
+use std::pin::Pin;
+use std::task::Poll;
+use tower::{Layer, Service};
 use tower_http::cors::CorsLayer;
 use tracing::debug;
 
@@ -14,13 +22,13 @@ pub struct Args {
 }
 
 pub async fn handle_command(args: Args) -> Result<()> {
-    let cors = CorsLayer::very_permissive();
-
     // build our application with a route
     let app = Router::new()
         .route("/users/:user_name", get(handlers::get_user))
         .route("/users", post(handlers::create_user))
-        .layer(cors);
+        .layer(CorsLayer::very_permissive())
+        // .layer(TraceLayer::new_for_http())
+        .layer(OtlpLayer::new());
 
     let server = axum::Server::try_bind(&args.listen_address)
         .with_context(|| format!("failed to bind to {}", args.listen_address))?
@@ -36,4 +44,65 @@ pub async fn handle_command(args: Args) -> Result<()> {
 async fn shutdown_signal() {
     tokio::signal::ctrl_c().await.unwrap();
     debug!("Received shutdown signal");
+}
+
+#[derive(Debug, Clone)]
+pub struct Otlp<S> {
+    inner: S,
+}
+
+impl<S> Otlp<S> {
+    fn new(inner: S) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T, Request> Service<Request> for Otlp<T>
+where
+    T: Service<Request>,
+    T::Future: 'static,
+    T::Error: Into<Box<dyn Error + Send + Sync>> + 'static,
+    T::Response: 'static,
+{
+    type Response = T::Response;
+    type Error = Box<dyn Error + Send + Sync>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<std::result::Result<(), Self::Error>> {
+        self.inner.poll_ready(cx).map_err(Into::into)
+    }
+
+    fn call(&mut self, req: Request) -> Self::Future {
+        let fut = self.inner.call(req);
+
+        // create a response in a future.
+        let f = async move {
+            let resp = fut;
+
+            resp.await.map_err(|err| err.into())
+        };
+
+        // Return the response as an immediate future
+        Box::pin(f)
+    }
+}
+
+#[derive(Clone)]
+pub struct OtlpLayer {}
+
+impl OtlpLayer {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<S> Layer<S> for OtlpLayer {
+    type Service = Otlp<S>;
+
+    fn layer(&self, service: S) -> Self::Service {
+        Otlp::new(service)
+    }
 }
--- user_service/src/main.rs
+++ user_service/src/main.rs
@@ -1,13 +1,17 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
 use opentelemetry::sdk::{trace, Resource};
 use opentelemetry::KeyValue;
 use opentelemetry_otlp::WithExportConfig;
+use std::io;
 use std::process;
+use std::process::ExitCode;
+use tracing::error;
 use tracing::{debug, error};
 use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Layer, Registry};
 use url::Url;
 
 mod client;
@@ -21,13 +25,17 @@ struct Application {
     #[command(subcommand)]
     command: SubCommands,
 
+    /// Log using JSON.
+    #[clap(long, env = "LOG_JSON")]
+    json: bool,
+
     /// Enable tracing
     #[clap(long, env)]
     tracing: bool,
 
     /// Endpoint of the OTLP collector
     #[clap(long, env, default_value = "http://localhost:4317")]
-    otlp_endpoint: Option<Url>,
+    otlp_endpoint: Url,
 }
 
 #[derive(Subcommand)]
@@ -40,58 +48,72 @@ enum SubCommands {
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> ExitCode {
     let app = Application::parse();
 
-    init_logging(&app).await;
+    let result = init_logging(&app);
+    if let Err(err) = result {
+        error!(%err, "unable to initialize logging");
+        return ExitCode::FAILURE;
+    }
 
     let result = match app.command {
         SubCommands::Client(args) => commands::client::handle_command(args).await,
         SubCommands::Start(args) => commands::start::handle_command(args).await,
     };
 
-    match result {
-        Ok(_) => debug!("Command completed successfully"),
-        Err(e) => {
-            error!("Command failed: {:#}", e);
-            process::exit(1)
-        }
+    if let Err(e) = result {
+        error!("Unable to initialize logging: {:#}", e);
+        return ExitCode::FAILURE;
     }
 
-    Ok(())
+    ExitCode::SUCCESS
 }
 
-async fn init_logging(app: &Application) {
-    let subscriber = tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .finish();
+fn init_logging(app: &Application) -> Result<()> {
+    // The filter layer controls which log levels to display.
+    let filter_layer = EnvFilter::from_default_env();
 
-    if app.tracing {
-        let tracer =
-            opentelemetry_otlp::new_pipeline()
-                .tracing()
-                .with_exporter(
-                    opentelemetry_otlp::new_exporter()
-                        .tonic()
-                        .with_endpoint("http://localhost:4317"),
-                )
-                .with_trace_config(trace::config().with_resource(Resource::new(vec![
-                    KeyValue::new("service.name", "user_service"),
-                ])))
-                .install_batch(opentelemetry::runtime::Tokio)
-                .expect("unable to install tracer");
-
-        // This converts traces from the tracing crate to the tracer specified above.
-        let otel_layer = OpenTelemetryLayer::new(tracer);
-
-        // Finally create a subscriber and append the OpenTelemetry layer to it.
-        let subscriber = subscriber.with(otel_layer);
-        tracing::subscriber::set_global_default(subscriber)
-            .expect("unable to set default subscriber");
-
-        debug!("Enabling tracing support");
+    // The log layer controls the output of log events to stderr. Depending on the
+    // `json` flag, it will either be human readable or json encoded.
+    let log_layer = tracing_subscriber::fmt::layer().with_writer(io::stderr);
+    let log_layer = if app.json {
+        log_layer.json().boxed()
     } else {
-        tracing::subscriber::set_global_default(subscriber)
-            .expect("unable to set default subscriber");
-    }
+        log_layer.boxed()
+    };
+
+    // The trace layer will send traces to the configured tracing backend
+    // depending on the `tracing` flag.
+    let trace_layer = if app.tracing {
+        // This tracer is responsible for sending the actual traces.
+        let tracer = opentelemetry_otlp::new_pipeline()
+            .tracing()
+            .with_exporter(
+                opentelemetry_otlp::new_exporter()
+                    .tonic()
+                    .with_endpoint(app.otlp_endpoint.to_string()),
+            )
+            .with_trace_config(
+                trace::config()
+                    .with_resource(Resource::new(vec![KeyValue::new("service.name", "api")])),
+            )
+            .install_batch(opentelemetry::runtime::Tokio)
+            .context("unable to install tracer: {err}")?;
+
+        // This layer will take the traces from the `tracing` crate and send
+        // them to the tracer specified above.
+        Some(OpenTelemetryLayer::new(tracer))
+    } else {
+        None
+    };
+
+    Registry::default()
+        .with(filter_layer)
+        .with(log_layer)
+        .with(trace_layer)
+        .try_init()
+        .context("unable to initialize logger")?;
+
+    Ok(())
 }
